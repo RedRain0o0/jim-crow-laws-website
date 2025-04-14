52254e5ce4d6c81069509aea1d5955d24cd03c6d
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-04-14 13:50:18 -0400

    update the website :3

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+function changeContent(content) {
+  content = document.getElementById(content);
+  mainContent = document.getElementById('mainContent');
+  sourcesContent = document.getElementById('sourcesContent');
+  creditsContent = document.getElementById('creditsContent');
+  mainContent.style.display = "none";
+  sourcesContent.style.display = "none";
+  creditsContent.style.display = "none";
+  content.style.display = "block";
+}
