50c10282f2715bfc77dde6b5aa320fd8687f3d4e
RedRain0o0 <96890698+RedRain0o0@users.noreply.github.com>
2025-04-15 10:07:47 -0400

    dropdownTo function

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function changeContent(content) {
   creditsContent.style.display = "none";
   content.style.display = "block";
 }
+
+function dropdownTo(dropdown) {
+  dropdown = document.getElementById(dropdown);
+  p1Content = document.getElementById('p1Content');
+  p2Content = document.getElementById('p2Content');
+  p3Content = document.getElementById('p3Content');
+  mainContent = document.getElementById('mainContent');
+  sourcesContent = document.getElementById('sourcesContent');
+  creditsContent = document.getElementById('creditsContent');
+  mainContent.style.display = "block";
+  sourcesContent.style.display = "none";
+  creditsContent.style.display = "none";
+  p1Content.style.display = "none";
+  p2Content.style.display = "none";
+  p3Content.style.display = "none";
+  dropdown.style.display = "block";
+}
